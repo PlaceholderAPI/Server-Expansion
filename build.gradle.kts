c97aae1114913ff2bcacbd48e0b75d2577e2408c
iGabyTM <contactgabytm@gmail.com>
2023-12-30 18:11:13 +0200

    build: set archiveFileName

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,9 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.3")
 }
+
+tasks {
+    shadowJar {
+        archiveFileName.set("PAPI-Expansion-Server-${project.version}.jar")
+    }
+}
