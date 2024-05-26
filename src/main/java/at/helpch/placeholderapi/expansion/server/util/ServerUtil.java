0c97efbf08987e2d28003dc2a33b987686b1ccce
iGabyTM <contactgabytm@gmail.com>
2024-05-26 17:59:21 +0300

    feat: use java 8 and spigot api SADLY

--- src/main/java/at/helpch/placeholderapi/expansion/server/util/ServerUtil.java
+++ src/main/java/at/helpch/placeholderapi/expansion/server/util/ServerUtil.java
@@ -4,19 +4,22 @@ import org.bukkit.Bukkit;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.TreeMap;
 
 public final class ServerUtil {
 
     private static final Map<String, String> variants = new TreeMap<>();
-    private static final String variant;
-    private static final String build;
 
     // TPS stuff
     private static final Object craftServer;
     private static final Field tpsField;
-    private static boolean hasTpsMethod; // Paper and its forks have Bukkit#getTps
+    private static Method getTpsMethod; // Paper and its forks have Bukkit#getTPS
+    // ----
+
+    private static final String variant;
+    private static final String build;
 
     static {
         variants.put("net.pl3x.purpur.PurpurConfig", "Purpur");
@@ -78,8 +81,7 @@ public final class ServerUtil {
 
     private static Field getTpsHandler() {
         try {
-            Bukkit.class.getMethod("getTPS");
-            hasTpsMethod = true;
+            getTpsMethod = Bukkit.class.getMethod("getTPS");
             return null;
         } catch (NoSuchMethodException ignored) { }
 
@@ -138,8 +140,12 @@ public final class ServerUtil {
     }
 
     public static double[] getTps() {
-        if (hasTpsMethod) {
-            return Bukkit.getTPS();
+        if (getTpsMethod != null) {
+            try {
+                return (double[]) getTpsMethod.invoke(null);
+            } catch (IllegalAccessException | InvocationTargetException ignored) {
+                return new double[]{0, 0, 0};
+            }
         }
 
         if (craftServer == null || tpsField == null) {
